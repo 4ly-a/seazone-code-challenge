eebddee4c8231af20612d31ce90f2fcdfe09061d
4ly-a <4ly.alcantara@gmail.com>
2021-05-18 17:49:44 -0300

    route components

---
+++ src/components/budgets/Budgets.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Budgets extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Orçamentos</h1>
+      </div>
+    );
+  }
+}
+
+export default Budgets;
---
+++ src/components/control/Control.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Control extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Controle</h1>
+      </div>
+    );
+  }
+}
+
+export default Control;
