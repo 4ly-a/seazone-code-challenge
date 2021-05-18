0368edd646896fa16a908354243fa41cd2724f9c
4ly-a <4ly.alcantara@gmail.com>
2021-05-18 20:12:32 -0300

    navbar

--- src/components/utils/Navbar.jsx
+++ src/components/utils/Navbar.jsx
@@ -1,29 +1,81 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Logo from "../../assets/img/logo/logo.svg";
 
 export default function Navbar() {
   return (
-    <div>
-      <ul>
-        <li>
-          <Link to="/">Seazone</Link>
+    <nav className="nav-container">
+      <ul className="nav-ul-container">
+        <li className="logo-container">
+          <Link
+            style={{ color: "inherit", textDecoration: "inherit" }}
+            className="link"
+            to="/"
+          >
+            <img className="logo" src={Logo} />
+          </Link>
         </li>
-        <li>
-          <Link to="/calendar">Calendário</Link>
-        </li>
-        <li>
-          <Link to="/client">Clientes</Link>
-        </li>{" "}
-        <li>
-          <Link to="/budgets">Orçamentos</Link>
-        </li>
-        <li>
-          <Link to="/properties">Propeiedades</Link>
-        </li>
-        <li>
-          <Link to="/control">Controle</Link>
-        </li>
+
+        <div className="li-container">
+          <li className="calendar">
+            <Link
+              style={{ color: "inherit", textDecoration: "inherit" }}
+              className="link"
+              to="/calendar"
+            >
+              Calendário
+            </Link>
+          </li>
+        </div>
+
+        <div className="li-container">
+          <li className="client">
+            <Link
+              style={{ color: "inherit", textDecoration: "inherit" }}
+              className="link"
+              to="/client"
+            >
+              Clientes
+            </Link>
+          </li>
+        </div>
+
+        <div className="li-container">
+          <li className="budgets">
+            <Link
+              style={{ color: "inherit", textDecoration: "inherit" }}
+              className="link"
+              to="/budgets"
+            >
+              Orçamentos
+            </Link>
+          </li>
+        </div>
+
+        <div className="li-container">
+          <li className="properties">
+            <Link
+              style={{ color: "inherit", textDecoration: "inherit" }}
+              className="link"
+              to="/properties"
+            >
+              Propeiedades
+            </Link>
+          </li>
+        </div>
+
+        <div className="li-container">
+          <li className="control">
+            <Link
+              style={{ color: "inherit", textDecoration: "inherit" }}
+              className="link"
+              to="/control"
+            >
+              Controle
+            </Link>
+          </li>
+        </div>
       </ul>
-    </div>
+    </nav>
   );
 }
