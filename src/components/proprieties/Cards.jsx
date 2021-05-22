9018ac218c87897d228601035f3d82fc9602ce12
4ly-a <4ly.alcantara@gmail.com>
2021-05-22 12:30:01 -0300

    create cards components and for loop to add n cards

--- src/components/proprieties/Cards.jsx
+++ src/components/proprieties/Cards.jsx
@@ -1,53 +1,59 @@
 import React from "react";
+import airbnb from "../../assets/img/circleLogo/airbnb.png";
+import house from "../../assets/img/circleLogo/house.png";
 
-const Cards = () => {
-  const [imoveis, diaria, taxaLimpeza, coucao] = [200, 120, 70, 10];
-  const total = imoveis + diaria + taxaLimpeza + coucao;
+const Cards = (props) => {
+  const total = props.diaria + props.taxaLimpeza + props.coucao;
   return (
-    <div className="card-container">
-      <div className="card">
-        <div className="image-container">
-          <img src="https://source.unsplash.com/500x500/?House" />
-          <div className="pins"></div>
-        </div>
+    <div className="card">
+      <div className="image-container">
+        <img className="house" alt="house" src={props.source} />
 
-        <h1 className="card-name">
-          JBM02 | Suíte Top Vista Para O Mar, resort
-        </h1>
-        <p className="card-info">{imoveis} imóveis</p>
-
-        <div className="table-container">
-          <table>
-            <tr className="1-container">
-              <td className="txt title">Valor da diária</td>
-              <td className="txt value">R$: {diaria},00</td>
-            </tr>
-            <tr className="2-container">
-              <td className="txt title">Total 5 dias</td>
-              <td className="txt value">R$: {diaria * 5},00</td>
-            </tr>
-            <tr className="3-container">
-              <td className="txt title">Taxa de limpeza</td>
-              <td className="txt value">R$: {taxaLimpeza},00</td>
-            </tr>
-            <tr className="4-container">
-              <td className="txt title">Coução</td>
-              <td className="txt value">R$: {coucao},00 </td>
-            </tr>
-            <tr className="5-container">
-              <td className="txt title" id="total">
-                Total
-              </td>
-              <td className="txt value" id="total-value">
-                R$: {total},00 |
-                <spam className="txt" id="a-vista">
-                  {" "}
-                  R$: {total - total * 0.1},00 à vista
-                </spam>
-              </td>
-            </tr>
-          </table>
+        <div className="circle-container">
+          <img className="pin airbnb" alt="airbnb" src={airbnb} />
+          <img className="pin airbnb" alt="airbnb" src={house} />
         </div>
+      </div>
+
+      <h1 className="card-name">JBM02 | Suíte Top Vista Para O Mar, resort</h1>
+      <p className="card-info">
+        {" "}
+        Jurerê Internacional . {props.hospedes} hóspedes . {props.quartos}{" "}
+        quartos
+      </p>
+
+      <div className="table-container">
+        <table>
+          <tr className="1-container">
+            <td className="txt title">Valor da diária</td>
+            <td className="txt value">R$: {props.diaria},00</td>
+          </tr>
+          <tr className="2-container">
+            <td className="txt title">Total 5 dias</td>
+            <td className="txt value">R$: {props.diaria * 5},00</td>
+          </tr>
+          <tr className="3-container">
+            <td className="txt title">Taxa de limpeza</td>
+            <td className="txt value">R$: {props.taxaLimpeza},00</td>
+          </tr>
+          <tr className="4-container">
+            <td className="txt title">Coução</td>
+            <td className="txt value">R$: {props.coucao},00 </td>
+          </tr>
+          <tr className="5-container">
+            <td className="txt title" id="total">
+              Total
+            </td>
+            <td className="txt value" id="total-value">
+              R$: {total},00 |
+              <br />
+              <spam className="txt" id="a-vista">
+                {" "}
+                R$: {total - total * 0.1},00 <br /> à vista
+              </spam>
+            </td>
+          </tr>
+        </table>
       </div>
     </div>
   );
--- src/components/proprieties/Properties.jsx
+++ src/components/proprieties/Properties.jsx
@@ -2,17 +2,42 @@ import React, { Component } from "react";
 import Cards from "./Cards";
 class Properties extends Component {
   render() {
+    function random(e) {
+      if (e === 0) {
+        return Math.floor(Math.random() * 10) + 1;
+      }
+      return Math.floor(Math.random() * e * 500) + 1;
+    }
+    const limit = 20;
+    let nCards = [];
+    for (let i = 1; i < limit; i++) {
+      nCards.push(
+        <Cards
+          diaria={random(i)}
+          taxaLimpeza={random(i)}
+          coucao={random(i)}
+          hospedes={random(0)}
+          quartos={random(0)}
+          source={`https://source.unsplash.com/${500 + i}x${500 + i}/?House`}
+        />
+      );
+    }
+
     return (
       <div className="proprieties-container">
         <div className="title-p-container">
-          <h1>Todos os imóveis</h1>
-          <p>200 imóveis</p>
+          <h1>
+            Todos os imóveis <br /> <spam> {limit} imóveis </spam>
+          </h1>
 
           <div className="ordenation-container">
             <label className="ordenation" htmlFor="ordenation"></label>
 
             <div className="order-button-container">
               <select className="b-default ordenation" name="ordenation" id="">
+                <option selected default disabled value="A-Z">
+                  Ordenar
+                </option>
                 <option value="A-Z">A-Z</option>
                 <option value="Z-A">Z-A</option>
                 <option value="maior-preco">Maior preço</option>
@@ -22,7 +47,7 @@ class Properties extends Component {
           </div>
         </div>
 
-        <Cards />
+        <div className="card-container">{nCards}</div>
       </div>
     );
   }
